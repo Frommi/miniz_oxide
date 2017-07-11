4cfd320c243a610cf2c6045c46ccf0933f6b0577
Frommi <daniil.liferenko@gmail.com>
2017-07-11 12:47:29 +0300

    deflate init oxide

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,12 @@ extern crate libc;
 use std::slice;
 use self::libc::*;
 use std::ptr;
+use std::mem;
 
 mod tdef;
 pub use tdef::tdefl_radix_sort_syms;
-
+pub use tdef::tdefl_compressor;
+pub use tdef::tdefl_put_buf_func_ptr;
 
 pub const MZ_ADLER32_INIT: c_ulong = 1;
 
@@ -55,12 +57,15 @@ pub fn mz_adler32_oxide(adler: c_ulong, data: &[u8]) -> c_ulong {
     (s2 << 16) + s1
 }
 
-
+#[allow(bad_style)]
 pub enum mz_internal_state {}
-pub type mz_alloc_func = extern "C" fn(*mut c_void, size_t, size_t) -> *mut c_void;
-pub type mz_free_func = extern "C" fn(*mut c_void, *mut c_void);
+#[allow(bad_style)]
+pub type mz_alloc_func = unsafe extern "C" fn(*mut c_void, size_t, size_t) -> *mut c_void;
+#[allow(bad_style)]
+pub type mz_free_func = unsafe extern "C" fn(*mut c_void, *mut c_void);
 
 #[repr(C)]
+#[allow(bad_style)]
 pub struct mz_stream {
     pub next_in: *const u8,
     pub avail_in: c_uint,
@@ -117,21 +122,28 @@ impl<'s> StreamOxide<'s> {
     }
 }
 
-
+#[allow(bad_style)]
 extern {
-    pub fn mz_deflateInit(stream: *mut mz_stream, level: c_int) -> c_int;
+    pub fn miniz_def_alloc_func(opaque: *mut c_void, items: size_t, size: size_t) -> *mut c_void;
+    pub fn miniz_def_free_func(opaque: *mut c_void, address: *mut c_void);
+
     pub fn mz_deflate(stream: *mut mz_stream, flush: c_int) -> c_int;
     pub fn mz_deflateEnd(stream: *mut mz_stream) -> c_int;
     pub fn mz_compressBound(source_len: c_ulong) -> c_ulong;
     pub fn mz_uncompress(pDest: *mut u8, pDest_len: *mut c_ulong, pSource: *const u8, source_len: c_ulong) -> c_int;
+
+    pub fn tdefl_create_comp_flags_from_zip_params(level: c_int, window_bits: c_int, strategy: c_int) -> c_uint;
+    pub fn tdefl_init(d: *mut tdefl_compressor, pPut_buf_func: Option<tdefl_put_buf_func_ptr>, pPut_buf_user: *mut c_void, flags: c_int) -> c_int;
 }
 
 #[no_mangle]
+#[allow(bad_style)]
 pub unsafe extern "C" fn mz_compress(pDest: *mut u8, pDest_len: *mut c_ulong, pSource: *const u8, source_len: c_ulong) -> c_int {
     mz_compress2(pDest, pDest_len, pSource, source_len, MZ_DEFAULT_COMPRESSION)
 }
 
 #[no_mangle]
+#[allow(bad_style)]
 pub unsafe extern "C" fn mz_compress2(pDest: *mut u8, pDest_len: *mut c_ulong, pSource: *const u8, source_len: c_ulong, level: c_int) -> c_int {
     assert!(!pDest_len.is_null());
     if (source_len | *pDest_len) > 0xFFFFFFFF {
@@ -167,3 +179,104 @@ pub fn mz_compress2_oxide(stream_oxide: &mut StreamOxide, level: c_int) -> c_int
 
     unsafe { mz_deflateEnd(stream_oxide.reduce()) }
 }
+
+
+#[no_mangle]
+#[allow(bad_style)]
+pub unsafe extern "C" fn mz_deflateInit(stream: *mut mz_stream, level: c_int) -> c_int {
+    mz_deflateInit2(stream, level, MZ_DEFLATED, MZ_DEFAULT_WINDOW_BITS, 9, MZ_DEFAULT_STRATEGY)
+}
+
+#[no_mangle]
+#[allow(bad_style)]
+pub unsafe extern "C" fn mz_deflateInit2(stream: *mut mz_stream, level: c_int, method: c_int,
+                                         window_bits: c_int, mem_level: c_int, strategy: c_int) -> c_int {
+    if stream.is_null() {
+        return MZ_STREAM_ERROR;
+    }
+    let mut stream_oxide = StreamOxide { stream: &mut *stream };
+    mz_deflate_init2_oxide(&mut stream_oxide, level, method, window_bits, mem_level, strategy)
+}
+
+pub use tdef::TDEFL_COMPUTE_ADLER32;
+pub use tdef::TDEFL_STATUS_OKAY;
+
+pub fn mz_deflate_init2_oxide(stream_oxide: &mut StreamOxide, level: c_int, method: c_int,
+                              window_bits: c_int, mem_level: c_int, strategy: c_int) -> c_int {
+    let comp_flags = TDEFL_COMPUTE_ADLER32 as u32 | unsafe { tdefl_create_comp_flags_from_zip_params(level, window_bits, strategy) };
+
+    if (method != MZ_DEFLATED) || ((mem_level < 1) || (mem_level > 9)) ||
+        ((window_bits != MZ_DEFAULT_WINDOW_BITS) && (-window_bits != MZ_DEFAULT_WINDOW_BITS)) {
+        return MZ_PARAM_ERROR;
+    }
+
+    stream_oxide.reduce().data_type = 0;
+    stream_oxide.reduce().adler = MZ_ADLER32_INIT;
+    stream_oxide.reduce().msg = ptr::null();
+    stream_oxide.reduce().reserved = 0;
+    stream_oxide.reduce().total_in = 0;
+    stream_oxide.reduce().total_out = 0;
+
+    if stream_oxide.reduce().zalloc.is_none() {
+        stream_oxide.reduce().zalloc = Some(miniz_def_alloc_func);
+    }
+    if stream_oxide.reduce().zfree.is_none() {
+        stream_oxide.reduce().zfree = Some(miniz_def_free_func);
+    }
+
+    let comp = unsafe {
+        stream_oxide.reduce().zalloc.unwrap()(
+            stream_oxide.reduce().opaque,
+            1,
+            mem::size_of::<tdefl_compressor>()
+        ) as *mut tdefl_compressor
+    };
+
+    if comp.is_null() {
+        return MZ_MEM_ERROR;
+    }
+
+    stream_oxide.reduce().state = comp as *mut mz_internal_state;
+    if unsafe { tdefl_init(comp, None, ptr::null_mut(), comp_flags as c_int) } != TDEFL_STATUS_OKAY {
+        unsafe { mz_deflateEnd(stream_oxide.reduce()) };
+        return MZ_PARAM_ERROR;
+    }
+
+    MZ_OK
+}
+
+//int mz_deflateInit2(mz_streamp pStream, int level, int method, int window_bits, int mem_level, int strategy)
+//{
+//    tdefl_compressor *pComp;
+//    mz_uint comp_flags = TDEFL_COMPUTE_ADLER32 | tdefl_create_comp_flags_from_zip_params(level, window_bits, strategy);
+//
+//    if (!pStream)
+//        return MZ_STREAM_ERROR;
+//    if ((method != MZ_DEFLATED) || ((mem_level < 1) || (mem_level > 9)) || ((window_bits != MZ_DEFAULT_WINDOW_BITS) && (-window_bits != MZ_DEFAULT_WINDOW_BITS)))
+//        return MZ_PARAM_ERROR;
+//
+//    pStream->data_type = 0;
+//    pStream->adler = MZ_ADLER32_INIT;
+//    pStream->msg = NULL;
+//    pStream->reserved = 0;
+//    pStream->total_in = 0;
+//    pStream->total_out = 0;
+//    if (!pStream->zalloc)
+//        pStream->zalloc = miniz_def_alloc_func;
+//    if (!pStream->zfree)
+//        pStream->zfree = miniz_def_free_func;
+//
+//    pComp = (tdefl_compressor *)pStream->zalloc(pStream->opaque, 1, sizeof(tdefl_compressor));
+//    if (!pComp)
+//        return MZ_MEM_ERROR;
+//
+//    pStream->state = (struct mz_internal_state *)pComp;
+//
+//    if (tdefl_init(pComp, NULL, NULL, comp_flags) != TDEFL_STATUS_OKAY)
+//    {
+//        mz_deflateEnd(pStream);
+//        return MZ_PARAM_ERROR;
+//    }
+//
+//    return MZ_OK;
+//}
--- src/tdef.rs
+++ src/tdef.rs
@@ -6,6 +6,105 @@ use self::libc::*;
 use std::slice;
 use std::mem;
 
+#[allow(bad_style)]
+pub type tdefl_put_buf_func_ptr = unsafe extern "C" fn(*const c_void, c_int, *mut c_void);
+
+pub const TDEFL_STATUS_BAD_PARAM: c_int = -2;
+pub const TDEFL_STATUS_PUT_BUF_FAILED: c_int = -1;
+pub const TDEFL_STATUS_OKAY: c_int = 0;
+pub const TDEFL_STATUS_DONE: c_int = 1;
+
+pub const TDEFL_NO_FLUSH: c_int = 0;
+pub const TDEFL_SYNC_FLUSH: c_int = 2;
+pub const TDEFL_FULL_FLUSH: c_int = 3;
+pub const TDEFL_FINISH: c_int = 4;
+
+pub const TDEFL_LZ_CODE_BUF_SIZE: c_int = 64 * 1024;
+pub const TDEFL_OUT_BUF_SIZE: c_int = (TDEFL_LZ_CODE_BUF_SIZE * 13) / 10;
+pub const TDEFL_MAX_HUFF_SYMBOLS: c_int = 288;
+pub const TDEFL_LZ_HASH_BITS: c_int = 15;
+pub const TDEFL_LEVEL1_HASH_SIZE_MASK: c_int = 4095;
+pub const TDEFL_LZ_HASH_SHIFT: c_int = (TDEFL_LZ_HASH_BITS + 2) / 3;
+pub const TDEFL_LZ_HASH_SIZE: c_int = 1 << TDEFL_LZ_HASH_BITS;
+
+pub const TDEFL_MAX_HUFF_TABLES: c_int = 3;
+pub const TDEFL_MAX_HUFF_SYMBOLS_0: c_int = 288;
+pub const TDEFL_MAX_HUFF_SYMBOLS_1: c_int = 32;
+pub const TDEFL_MAX_HUFF_SYMBOLS_2: c_int = 19;
+pub const TDEFL_LZ_DICT_SIZE: c_int = 32768;
+pub const TDEFL_LZ_DICT_SIZE_MASK: c_int = TDEFL_LZ_DICT_SIZE - 1;
+pub const TDEFL_MIN_MATCH_LEN: c_int = 3;
+pub const TDEFL_MAX_MATCH_LEN: c_int = 258;
+
+pub const TDEFL_WRITE_ZLIB_HEADER: c_int = 0x01000;
+pub const TDEFL_COMPUTE_ADLER32: c_int = 0x02000;
+pub const TDEFL_GREEDY_PARSING_FLAG: c_int = 0x04000;
+pub const TDEFL_NONDETERMINISTIC_PARSING_FLAG: c_int = 0x08000;
+pub const TDEFL_RLE_MATCHES: c_int = 0x10000;
+pub const TDEFL_FILTER_MATCHES: c_int = 0x20000;
+pub const TDEFL_FORCE_ALL_STATIC_BLOCKS: c_int = 0x40000;
+pub const TDEFL_FORCE_ALL_RAW_BLOCKS: c_int = 0x80000;
+
+#[repr(C)]
+#[allow(bad_style)]
+pub struct tdefl_compressor {
+    m_pPut_buf_func: tdefl_put_buf_func_ptr,
+    m_pPut_buf_user: *mut c_void,
+
+    m_flags: c_uint,
+    m_max_probes: [c_uint; 2],
+
+    m_greedy_parsing: c_int,
+
+    m_adler32: c_uint,
+    m_lookahead_pos: c_uint,
+    m_lookahead_size: c_uint,
+    m_dict_size: c_uint,
+
+    m_pLZ_code_buf: *mut u8,
+    m_pLZ_flags: *mut u8,
+    m_pOutput_buf: *mut u8,
+    m_pOutput_buf_end: *mut u8,
+
+    m_num_flags_left: c_uint,
+    m_total_lz_bytes: c_uint,
+    m_lz_code_buf_dict_pos: c_uint,
+    m_bits_in: c_uint,
+    m_bit_buffer: c_uint,
+
+    m_saved_match_dist: c_uint,
+    m_saved_match_len: c_uint,
+    m_saved_lit: c_uint,
+    m_output_flush_ofs: c_uint,
+    m_output_flush_remaining: c_uint,
+    m_finished: c_uint,
+    m_block_index: c_uint,
+    m_wants_to_finish: c_uint,
+
+    m_prev_return_status: c_int,
+
+    m_pIn_buf: *const c_void,
+    m_pOut_buf: *mut c_void,
+    m_pIn_buf_size: *mut size_t,
+    m_pOut_buf_size: *mut size_t,
+
+    m_flush: c_int,
+
+    m_pSrc: *const u8,
+
+    m_src_buf_left: size_t,
+    m_out_buf_ofs: size_t,
+
+    m_dict: [u8; (TDEFL_LZ_DICT_SIZE + TDEFL_MAX_MATCH_LEN - 1) as usize],
+    m_huff_count: [[u16; TDEFL_MAX_HUFF_SYMBOLS as usize]; TDEFL_MAX_HUFF_TABLES as usize],
+    m_huff_codes: [[u16; TDEFL_MAX_HUFF_SYMBOLS as usize]; TDEFL_MAX_HUFF_TABLES as usize],
+    m_huff_code_sizes: [[u8; TDEFL_MAX_HUFF_SYMBOLS as usize]; TDEFL_MAX_HUFF_TABLES as usize],
+    m_lz_code_buf: [u8; TDEFL_LZ_CODE_BUF_SIZE as usize],
+    m_next: [u16; TDEFL_LZ_DICT_SIZE as usize],
+    m_hash: [u16; TDEFL_LZ_HASH_SIZE as usize],
+    m_output_buf: [u8; TDEFL_OUT_BUF_SIZE as usize],
+}
+
 #[repr(C)]
 #[derive(Copy, Clone)]
 #[allow(bad_style)]
