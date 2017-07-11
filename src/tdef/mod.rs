a22ffb8394493b936cb93f32bc188b3031405270
Frommi <daniil.liferenko@gmail.com>
2017-07-11 15:17:45 +0300

    tdef oxide file refactor

--- src/tdef/mod.rs
+++ src/tdef/mod.rs
@@ -2,6 +2,9 @@
 
 extern crate libc;
 
+mod tdef_oxide;
+pub use self::tdef_oxide::tdefl_radix_sort_syms_oxide;
+
 use self::libc::*;
 use std::slice;
 use std::mem;
@@ -119,40 +122,4 @@ pub unsafe extern "C" fn tdefl_radix_sort_syms(num_syms : c_uint, pSyms0: *mut t
     let syms0 = slice::from_raw_parts_mut(pSyms0, num_syms as usize);
     let syms1 = slice::from_raw_parts_mut(pSyms1, num_syms as usize);
     tdefl_radix_sort_syms_oxide(syms0, syms1).as_mut_ptr()
-}
-
-pub fn tdefl_radix_sort_syms_oxide<'a>(syms0: &'a mut [tdefl_sym_freq], syms1: &'a mut [tdefl_sym_freq]) -> &'a mut [tdefl_sym_freq] {
-    let mut hist = [[0; 256]; 2];
-
-    for freq in syms0.iter() {
-        hist[0][(freq.m_key & 0xFF) as usize] += 1;
-        hist[1][((freq.m_key >> 8) & 0xFF) as usize] += 1;
-    }
-
-    let mut n_passes = 2;
-    if syms0.len() == hist[1][0] {
-        n_passes -= 1;
-    }
-
-    let mut current_syms = syms0;
-    let mut new_syms = syms1;
-
-    for pass in 0..n_passes {
-        let mut offsets = [0; 256];
-        let mut offset = 0;
-        for i in 0..256 {
-            offsets[i] = offset;
-            offset += hist[pass][i];
-        }
-
-        for sym in current_syms.iter() {
-            let j = ((sym.m_key >> (pass * 8)) & 0xFF) as usize;
-            new_syms[offsets[j]] = *sym;
-            offsets[j] += 1;
-        }
-
-        mem::swap(&mut current_syms, &mut new_syms);
-    }
-
-    current_syms
 }
