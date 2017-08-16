3918b6bd80369a8f115448d098569e22ece69805
Frommi <daniil.liferenko@gmail.com>
2017-08-16 14:36:51 +0300

    all huffman functions to methods

--- src/tdef/tdef_oxide.rs
+++ src/tdef/tdef_oxide.rs
@@ -377,7 +377,125 @@ impl HuffmanOxide {
         }
     }
 
-    pub fn optimize_huffman_table(
+    pub fn radix_sort_symbols<'a>(
+        symbols0: &'a mut [SymFreq],
+        symbols1: &'a mut [SymFreq],
+    ) -> &'a mut [SymFreq] {
+        let mut hist = [[0; 256]; 2];
+
+        for freq in symbols0.iter() {
+            hist[0][(freq.key & 0xFF) as usize] += 1;
+            hist[1][((freq.key >> 8) & 0xFF) as usize] += 1;
+        }
+
+        let mut n_passes = 2;
+        if symbols0.len() == hist[1][0] {
+            n_passes -= 1;
+        }
+
+        let mut current_symbols = symbols0;
+        let mut new_symbols = symbols1;
+
+        for pass in 0..n_passes {
+            let mut offsets = [0; 256];
+            let mut offset = 0;
+            for i in 0..256 {
+                offsets[i] = offset;
+                offset += hist[pass][i];
+            }
+
+            for sym in current_symbols.iter() {
+                let j = ((sym.key >> (pass * 8)) & 0xFF) as usize;
+                new_symbols[offsets[j]] = *sym;
+                offsets[j] += 1;
+            }
+
+            mem::swap(&mut current_symbols, &mut new_symbols);
+        }
+
+        current_symbols
+    }
+
+    pub fn calculate_minimum_redundancy(symbols: &mut [SymFreq]) {
+        match symbols.len() {
+            0 => (),
+            1 => symbols[0].key = 1,
+            n => {
+                symbols[0].key += symbols[1].key;
+                let mut root = 0;
+                let mut leaf = 2;
+                for next in 1..n - 1 {
+                    if (leaf >= n) || (symbols[root].key < symbols[leaf].key) {
+                        symbols[next].key = symbols[root].key;
+                        symbols[root].key = next as u16;
+                        root += 1;
+                    } else {
+                        symbols[next].key = symbols[leaf].key;
+                        leaf += 1;
+                    }
+
+                    if (leaf >= n) || (root < next && symbols[root].key < symbols[leaf].key) {
+                        symbols[next].key = symbols[next].key.wrapping_add(symbols[root].key);
+                        symbols[root].key = next as u16;
+                        root += 1;
+                    } else {
+                        symbols[next].key = symbols[next].key.wrapping_add(symbols[leaf].key);
+                        leaf += 1;
+                    }
+                }
+
+                symbols[n - 2].key = 0;
+                for next in (0..n - 2).rev() {
+                    symbols[next].key = symbols[symbols[next].key as usize].key + 1;
+                }
+
+                let mut avbl = 1;
+                let mut used = 0;
+                let mut dpth = 0;
+                let mut root = (n - 2) as i32;
+                let mut next = (n - 1) as i32;
+                while avbl > 0 {
+                    while (root >= 0) && (symbols[root as usize].key == dpth) {
+                        used += 1;
+                        root -= 1;
+                    }
+                    while avbl > used {
+                        symbols[next as usize].key = dpth;
+                        next -= 1;
+                        avbl -= 1;
+                    }
+                    avbl = 2 * used;
+                    dpth += 1;
+                    used = 0;
+                }
+            }
+        }
+    }
+
+    pub fn enforce_max_code_size(
+        num_codes: &mut [i32],
+        code_list_len: usize,
+        max_code_size: usize,
+    ) {
+        if code_list_len <= 1 { return; }
+
+        num_codes[max_code_size] += num_codes[max_code_size + 1..].iter().sum();
+        let total = num_codes[1..max_code_size + 1].iter().rev().enumerate()
+            .fold(0u32, |total, (i, &x)| total + ((x as u32) << i));
+
+        for _ in (1 << max_code_size)..total {
+            num_codes[max_code_size] -= 1;
+            for i in (1..max_code_size).rev() {
+                if num_codes[i] != 0 {
+                    num_codes[i] -= 1;
+                    num_codes[i + 1] += 2;
+                    break;
+                }
+            }
+        }
+    }
+
+    pub fn optimize_table(
         &mut self,
         table_num: usize,
         table_len: usize,
@@ -406,17 +524,17 @@ impl HuffmanOxide {
                 }
             }
 
-            let mut symbols = radix_sort_syms(
+            let mut symbols = Self::radix_sort_symbols(
                 &mut symbols0[..num_used_symbols],
                 &mut symbols1[..num_used_symbols],
             );
-            calculate_minimum_redundancy(symbols);
+            Self::calculate_minimum_redundancy(symbols);
 
             for symbol in symbols.iter() {
                 num_codes[symbol.key as usize] += 1;
             }
 
-            huffman_enforce_max_code_size(&mut num_codes, num_used_symbols, code_size_limit);
+            Self::enforce_max_code_size(&mut num_codes, num_used_symbols, code_size_limit);
 
             memset(&mut self.code_sizes[table_num][..], 0);
             memset(&mut self.codes[table_num][..], 0);
@@ -463,8 +581,8 @@ impl HuffmanOxide {
 
         memset(&mut self.code_sizes[1][..32], 5);
 
-        self.optimize_huffman_table(0, 288, 15, true);
-        self.optimize_huffman_table(1, 32, 15, true);
+        self.optimize_table(0, 288, 15, true);
+        self.optimize_table(1, 32, 15, true);
 
         output.put_bits(1, 2)
     }
@@ -472,8 +590,8 @@ impl HuffmanOxide {
     pub fn start_dynamic_block(&mut self, output: &mut OutputBufferOxide) -> io::Result<()> {
         self.count[0][256] = 1;
 
-        self.optimize_huffman_table(0, TDEFL_MAX_HUFF_SYMBOLS_0, 15, false);
-        self.optimize_huffman_table(1, TDEFL_MAX_HUFF_SYMBOLS_1, 15, false);
+        self.optimize_table(0, TDEFL_MAX_HUFF_SYMBOLS_0, 15, false);
+        self.optimize_table(1, TDEFL_MAX_HUFF_SYMBOLS_1, 15, false);
 
         let num_lit_codes = 286 - &self.code_sizes[0][257..286]
             .iter().rev().take_while(|&x| *x == 0).count();
@@ -531,7 +649,7 @@ impl HuffmanOxide {
             rle.zero_code_size(&mut packed_code_sizes_cursor, self)?;
         }
 
-        self.optimize_huffman_table(2, TDEFL_MAX_HUFF_SYMBOLS_2, 7, false);
+        self.optimize_table(2, TDEFL_MAX_HUFF_SYMBOLS_2, 7, false);
 
         output.put_bits(2, 2)?;
 
@@ -775,124 +893,6 @@ impl LZOxide {
         if self.num_flags_left == 0 {
             self.num_flags_left = 8;
             self.plant_flag();
-        }
-    }
-}
-
-pub fn radix_sort_syms<'a>(
-    symbols0: &'a mut [SymFreq],
-    symbols1: &'a mut [SymFreq],
-) -> &'a mut [SymFreq] {
-    let mut hist = [[0; 256]; 2];
-
-    for freq in symbols0.iter() {
-        hist[0][(freq.key & 0xFF) as usize] += 1;
-        hist[1][((freq.key >> 8) & 0xFF) as usize] += 1;
-    }
-
-    let mut n_passes = 2;
-    if symbols0.len() == hist[1][0] {
-        n_passes -= 1;
-    }
-
-    let mut current_symbols = symbols0;
-    let mut new_symbols = symbols1;
-
-    for pass in 0..n_passes {
-        let mut offsets = [0; 256];
-        let mut offset = 0;
-        for i in 0..256 {
-            offsets[i] = offset;
-            offset += hist[pass][i];
-        }
-
-        for sym in current_symbols.iter() {
-            let j = ((sym.key >> (pass * 8)) & 0xFF) as usize;
-            new_symbols[offsets[j]] = *sym;
-            offsets[j] += 1;
-        }
-
-        mem::swap(&mut current_symbols, &mut new_symbols);
-    }
-
-    current_symbols
-}
-
-pub fn calculate_minimum_redundancy(symbols: &mut [SymFreq]) {
-    match symbols.len() {
-        0 => (),
-        1 => symbols[0].key = 1,
-        n => {
-            symbols[0].key += symbols[1].key;
-            let mut root = 0;
-            let mut leaf = 2;
-            for next in 1..n - 1 {
-                if (leaf >= n) || (symbols[root].key < symbols[leaf].key) {
-                    symbols[next].key = symbols[root].key;
-                    symbols[root].key = next as u16;
-                    root += 1;
-                } else {
-                    symbols[next].key = symbols[leaf].key;
-                    leaf += 1;
-                }
-
-                if (leaf >= n) || (root < next && symbols[root].key < symbols[leaf].key) {
-                    symbols[next].key = symbols[next].key.wrapping_add(symbols[root].key);
-                    symbols[root].key = next as u16;
-                    root += 1;
-                } else {
-                    symbols[next].key = symbols[next].key.wrapping_add(symbols[leaf].key);
-                    leaf += 1;
-                }
-            }
-
-            symbols[n - 2].key = 0;
-            for next in (0..n - 2).rev() {
-                symbols[next].key = symbols[symbols[next].key as usize].key + 1;
-            }
-
-            let mut avbl = 1;
-            let mut used = 0;
-            let mut dpth = 0;
-            let mut root = (n - 2) as i32;
-            let mut next = (n - 1) as i32;
-            while avbl > 0 {
-                while (root >= 0) && (symbols[root as usize].key == dpth) {
-                    used += 1;
-                    root -= 1;
-                }
-                while avbl > used {
-                    symbols[next as usize].key = dpth;
-                    next -= 1;
-                    avbl -= 1;
-                }
-                avbl = 2 * used;
-                dpth += 1;
-                used = 0;
-            }
-        }
-    }
-}
-
-pub fn huffman_enforce_max_code_size(
-    num_codes: &mut [i32],
-    code_list_len: usize,
-    max_code_size: usize,
-) {
-    if code_list_len <= 1 { return; }
-
-    num_codes[max_code_size] += num_codes[max_code_size + 1..].iter().sum();
-    let total = num_codes[1..max_code_size + 1].iter().rev().enumerate()
-        .fold(0u32, |total, (i, &x)| total + ((x as u32) << i));
-
-    for _ in (1 << max_code_size)..total {
-        num_codes[max_code_size] -= 1;
-        for i in (1..max_code_size).rev() {
-            if num_codes[i] != 0 {
-                num_codes[i] -= 1;
-                num_codes[i + 1] += 2;
-                break;
-            }
         }
     }
 }
