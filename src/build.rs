3397a600bbb5341d7d6895bc8a063ba0a33fa7c8
Frommi <daniil.liferenko@gmail.com>
2017-10-10 16:41:11 +0300

    return miniz_zip for test.sh

--- src/build.rs
+++ src/build.rs
@@ -1,18 +1,21 @@
-#[cfg(not(feature = "build_orig_miniz"))]
+#[cfg(feature = "build_stub_miniz")]
 extern crate cc;
 
-#[cfg(not(feature = "build_orig_miniz"))]
+#[cfg(not(any(feature = "build_stub_miniz", feature = "build_orig_miniz")))]
+fn main() {}
+
+#[cfg(feature = "build_stub_miniz")]
 fn main() {
-//    cc::Build::new()
-//        .files(
-//            &[
-//                "miniz_stub/miniz.c",
-//                "miniz_stub/miniz_zip.c",
-//                "miniz_stub/miniz_tinfl.c",
-//                "miniz_stub/miniz_tdef.c",
-//            ],
-//        )
-//        .compile("libminiz.a");
+    cc::Build::new()
+        .files(
+            &[
+                "miniz_stub/miniz.c",
+                "miniz_stub/miniz_zip.c",
+                "miniz_stub/miniz_tinfl.c",
+                "miniz_stub/miniz_tdef.c",
+            ],
+        )
+        .compile("libminiz.a");
 }
 
 #[cfg(feature = "build_orig_miniz")]
