3f34da1a025093fe928392e432a176f58baca623
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2019-06-23 07:36:10 +0200

    Fix buffer overflow bug in deflate::core::BitBuffer::flush
    The problem is that this function does not check that there is enough space to fit a u64, it only checks that the starting position for the write is in bounds.
    I have tried adding `assert!(pos.checked_add(8).unwrap() <= output.inner.get_ref().len());` before the unsafe part, but that regressed performance exactly as much as the fully safe version, so I went for the fully safe code.

--- miniz_oxide/src/deflate/core.rs
+++ miniz_oxide/src/deflate/core.rs
@@ -570,11 +570,9 @@ impl BitBuffer {
 
     fn flush(&mut self, output: &mut OutputBufferOxide) -> io::Result<()> {
         let pos = output.inner.position() as usize;
-        let inner = &mut ((*output.inner.get_mut())[pos]) as *mut u8 as *mut u64;
-        // # Unsafe
-        // TODO: check unsafety
-        unsafe {
-            ptr::write_unaligned(inner, self.bit_buffer.to_le());
+        { // isolation to please borrow checker
+            let inner = &mut (*output.inner.get_mut())[pos..pos+8];
+            inner.copy_from_slice(&self.bit_buffer.to_le_bytes());
         }
         output.inner.seek(
             SeekFrom::Current((self.bits_in >> 3) as i64),
