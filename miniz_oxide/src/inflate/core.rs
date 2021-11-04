012c9289e7297d8a821a638d81f46a99838eb963
Matthew D. Fuller <fullermd@over-yonder.net>
2021-11-04 18:27:14 -0500

    Document the TINFL_FLAG's.

--- miniz_oxide/src/inflate/core.rs
+++ miniz_oxide/src/inflate/core.rs
@@ -102,23 +102,46 @@ const LITLEN_TABLE: usize = 0;
 const DIST_TABLE: usize = 1;
 const HUFFLEN_TABLE: usize = 2;
 
+/// Flags to [`decompress()`] to control how inflation works.
+///
+/// These define bits for a bitmask argument.
 pub mod inflate_flags {
     /// Should we try to parse a zlib header?
+    ///
+    /// If unset, [`decompress()`] will expect an RFC1951 deflate stream.  If set, it will expect an
+    /// RFC1950 zlib wrapper around the deflate stream.
     pub const TINFL_FLAG_PARSE_ZLIB_HEADER: u32 = 1;
-    /// There is more input that hasn't been given to the decompressor yet.
+
+    /// There will be more input that hasn't been given to the decompressor yet.
+    ///
+    /// This is useful when you want to decompress what you have so far,
+    /// even if you know there is probably more input that hasn't gotten here yet (_e.g._, over a
+    /// network connection).  When [`decompress()`][super::decompress] reaches the end of the input
+    /// without finding the end of the compressed stream, it will return
+    /// [`TINFLStatus::NeedsMoreInput`][super::TINFLStatus::NeedsMoreInput] if this is set,
+    /// indicating that you should get more data before calling again.  If not set, it will return
+    /// [`TINFLStatus::FailedCannotMakeProgress`][super::TINFLStatus::FailedCannotMakeProgress]
+    /// suggesting the stream is corrupt, since you claimed it was all there.
     pub const TINFL_FLAG_HAS_MORE_INPUT: u32 = 2;
+
     /// The output buffer should not wrap around.
     pub const TINFL_FLAG_USING_NON_WRAPPING_OUTPUT_BUF: u32 = 4;
-    /// Should we calculate the adler32 checksum of the output data even if we're not inflating a
-    /// zlib stream?
+
+    /// Calculate the adler32 checksum of the output data even if we're not inflating a zlib stream.
     ///
-    /// NOTE: Enable/disabling this between calls to decompress will result in an incorect checksum.
+    /// If [`TINFL_FLAG_IGNORE_ADLER32`] is specified, it will override this.
+    ///
+    /// NOTE: Enabling/disabling this between calls to decompress will result in an incorect
+    /// checksum.
     pub const TINFL_FLAG_COMPUTE_ADLER32: u32 = 8;
-    /// Should we ignore adler32 checksum even if we are inflating a zlib stream.
-    /// Overrides TINFL_FLAG_COMPUTE_ADLER32 if both are enabled.
+
+    /// Ignore adler32 checksum even if we are inflating a zlib stream.
+    ///
+    /// Overrides [`TINFL_FLAG_COMPUTE_ADLER32`] if both are enabled.
     ///
     /// NOTE: This flag does not exist in miniz as it does not support this and is a
     /// custom addition for miniz_oxide.
+    ///
     /// NOTE: Should not be changed from enabled to disabled after decompression has started,
     /// this will result in checksum failure (outside the unlikely event where the checksum happens
     /// to match anyway).
