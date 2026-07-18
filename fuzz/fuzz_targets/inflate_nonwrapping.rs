0fa8ad204484c41906829b11953d0d76947eff4a
xingyaner <longfor2025@gmail.com>
2026-07-18 22:16:52 +0800

    Fix fuzz targets broken after b7a5908

--- fuzz/fuzz_targets/inflate_nonwrapping.rs
+++ fuzz/fuzz_targets/inflate_nonwrapping.rs
@@ -1,9 +1,14 @@
 #![no_main]
 #[macro_use]
 extern crate libfuzzer_sys;
-extern crate miniz_oxide;
+extern crate flate2;
+
+use flate2::read::DeflateDecoder;
+use std::io::Read;
+
 
 fuzz_target!(|data: &[u8]| {
     // fuzzed code goes here
-    let _ = DeflateDecoder::new(data).read_to_end(&mut result),
+    let mut result = Vec::new();
+    let _ = DeflateDecoder::new(data).read_to_end(&mut result);
 });
